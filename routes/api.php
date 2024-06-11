561a4b39bf34915882da002f06f4f0b4696b9194
Neznamy-Samo <46057894+Neznamy-Samo@users.noreply.github.com>
2024-06-11 12:57:51 +0200

    auth_update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrganizerController;
 use App\Http\Controllers\SpeakerController;
 use App\Http\Controllers\TestimonalController;
@@ -31,7 +32,6 @@ Route::patch('/update-conference/{id}', [ConferenceController::class, 'updateCon
 Route::delete('/delete-conference/{id}', [ConferenceController::class, 'deleteConference']);
 
 Route::get('/get-gallery-images',[GalleryImageController::class,'getGalleryImages']);
-Route::get('/get-gallery-images-byGalleryId/{id}',[GalleryImageController::class,'getGalleryImagesByGalleryId']);
 Route::get('/get-gallery-image-byId/{id}', [GalleryImageController::class, 'getGalleryImageById']);
 Route::post('/create-gallery-image', [GalleryImageController::class, 'createGalleryImage']);
 Route::patch('/update-gallery-image/{id}', [GalleryImageController::class, 'updateGalleryImage']);
