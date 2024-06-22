edfd47a72cf99745e772e717fda3be08d439dd84
Nathaniel Fruni <fruninathan@gmail.com>
2024-06-22 18:32:01 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AddLocations::class,
             AddAddresses::class,
             AddConferences::class,
             AddOrganizers::class,
